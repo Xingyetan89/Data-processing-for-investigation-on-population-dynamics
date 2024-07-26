dbc9ac083c38dae6d764a0886994afaf31e95ce9
Xingyetan89 <xingye.tan@link.cuhk.edu.hk>
2024-07-26 12:39:34 +0800

    Add files via upload

---
+++ Fluctuation-prediction/Fs_estimation.m
@@ -0,0 +1,24 @@
+C = readmatrix('Avg_Fs.csv');
+k = size(C);
+AFS = C(2:k(1),2:k(2));
+r = C(2:k(1),1);
+ts = C(1,2:k(2));
+m = size(AFS);
+n = length(r);
+p = length(ts);
+R1 = [];
+R2 = [];
+for i = 1 : m(1)
+    x1 = AFS(i,:);
+    x2 = x1';
+    R1 = [R1; x2];
+end
+for i = 1 : n
+    for j = 1 : p
+        fs = -0.508 * log(r(i)) + 1.605 * log(ts(j)) - 0.0845 *  log(r(i)) * log(ts(j)) + 0.342;
+        R3 = [fs, r(i), ts(j)];
+        R2 = [R2; R3];
+    end
+end
+Results = [R1, R2];
+disp(Results);
